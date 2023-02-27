bc5526ecfecba20eb87aa95eb7275057cb2d10f9
DMZ1021 <diegomucheniski.gft@sulamerica.com.br>
2023-02-27 11:38:43 -0300

    Projeto com execucao de job manualmente

---
+++ src/main/java/com/example/springbatchkeycloakexample/api/controller/JobLaunchRequest.java
@@ -0,0 +1,35 @@
+package com.example.springbatchkeycloakexample.api.controller;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+
+import java.util.Properties;
+
+public class JobLaunchRequest {
+
+    private String name;
+    private Properties jobParameters;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Properties getJobParamsProperties() {
+        return jobParameters;
+    }
+
+    public void setJobParamsProperties(Properties jobParameters) {
+        this.jobParameters = jobParameters;
+    }
+
+    public JobParameters getJobParameters() {
+        Properties properties = new Properties();
+        properties.putAll(this.jobParameters);
+        return new JobParametersBuilder(properties).toJobParameters();
+    }
+
+}
