490b2de030bdbb43b6e2343e753bad81f59edff1
DMZ1021 <diegomucheniski.gft@sulamerica.com.br>
2023-02-27 12:07:58 -0300

    Agendamento personalizado

--- src/main/java/com/example/springbatchkeycloakexample/batch/schedule/UsuarioScheduleJobConfiguration.java
+++ src/main/java/com/example/springbatchkeycloakexample/batch/schedule/UsuarioScheduleJobConfiguration.java
@@ -12,12 +12,24 @@ public class UsuarioScheduleJobConfiguration {
         return JobBuilder.newJob(UsuarioBatchScheduleJob.class).storeDurably().build();
     }
 
+    /**
+     *
+     * O gatilho de execução configurado foi a cada 5 segundos,
+     * no máximo 4 execuções repetidas.
+     * Esse intervalo poderia ser customizado para qualquer periodicidade desejada,
+     * o SimpleScheduleBuilder do Quartz possui vários métodos para essa finalidade.
+     * https://www.quartz-scheduler.org/api/2.1.7/org/quartz/SimpleScheduleBuilder.html
+     */
     @Bean
     public Trigger jobTrigger() {
+
         SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder
                 .simpleSchedule()
-                .withIntervalInSeconds(5)
-                .withRepeatCount(4);
+                .withIntervalInSeconds(5) // intervalo em Segundos
+//                .withIntervalInHours(24) // intervalo em horas
+//                .withRepeatCount(4); // Quantidade de repetições
+                .repeatForever();
+
         return TriggerBuilder
                 .newTrigger()
                 .forJob(quartzJobDetail())
